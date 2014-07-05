782e582ae707e2c64babd6670fe1e6dfc1838aab
s_murakami <s.murakami@conol.jp>
2014-07-05 16:41:14 +0900

    各種ViewControllerを作成

---
+++ nanikore/SeeAnswerViewController.h
@@ -0,0 +1,13 @@
+//
+//  SeeAnswerViewController.h
+//  nanikore
+//
+//  Created by 村上 晋太郎 on 2014/07/05.
+//  Copyright (c) 2014年 nae-lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SeeAnswerViewController : UIViewController
+
+@end
