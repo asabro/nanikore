57a792b43f86ec783662492053d56e38a8b43653
s_murakami <s.murakami@conol.jp>
2014-07-05 16:15:03 +0900

    リファクター

--- nanikore/TopViewController.m
+++ nanikore/TopViewController.m
@@ -6,14 +6,14 @@
 //  Copyright (c) 2014 nae-lab. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "TopViewController.h"
 
-@interface ViewController ()
+@interface TopViewController ()
             
 
 @end
 
-@implementation ViewController
+@implementation TopViewController
             
 - (void)viewDidLoad {
     [super viewDidLoad];
