f487b1f4f182ef2847a2795938c805e91092568d
s_murakami <s.murakami@conol.jp>
2014-07-05 19:49:27 +0900

    askモードへの分割を実装

---
+++ nanikore/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  nanikore
+//
+//  Created by 村上 晋太郎 on 2014/07/05.
+//  Copyright (c) 2014年 nae-lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
