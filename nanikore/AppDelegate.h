55ecb568b8ec45d908b86e8908fa0c8b91c9616d
sidestepism <fushimi.ryohe.i@gmail.com>
2014-07-05 13:33:38 +0900

    Initial Commit

---
+++ nanikore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nanikore
+//
+//  Created by Ryohei Fushimi on 7/5/14.
+//  Copyright (c) 2014 nae-lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nanikore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nanikore
+//
+//  Created by Ryohei Fushimi on 7/5/14.
+//  Copyright (c) 2014 nae-lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
