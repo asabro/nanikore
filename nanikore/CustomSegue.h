1d43da75f3285fa9d05a1e58243a74e2e3e88c80
s_murakami <s.murakami@conol.jp>
2014-07-06 06:54:45 +0900

    画面のパッと切り替えを実現

---
+++ nanikore/CustomSegue.h
@@ -0,0 +1,13 @@
+//
+//  CustomSegue.h
+//  nanikore
+//
+//  Created by 村上 晋太郎 on 2014/07/06.
+//  Copyright (c) 2014年 nae-lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomSegue : UIStoryboardSegue
+
+@end
