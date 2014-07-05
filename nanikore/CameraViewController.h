1f4a7a1df47175a2b2bc16d9ba97ae48c0de00b7
s_murakami <s.murakami@conol.jp>
2014-07-05 15:11:44 +0900

    CameraViewControllerを追加

---
+++ nanikore/CameraViewController.h
@@ -0,0 +1,19 @@
+//
+//  CameraViewController.h
+//  nanikore
+//
+//  Created by 村上 晋太郎 on 2014/07/05.
+//  Copyright (c) 2014年 nae-lab. All rights reserved.
+//
+//  ====================
+//  CameraViewController
+//  ====================
+//
+//  写真撮影をするView
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+
+@end
