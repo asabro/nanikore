3fd504c8e0465102b6a67b7ea6defb945e13ce9e
sidestepism <fushimi.ryohe.i@gmail.com>
2014-07-05 15:33:34 +0900

    server mockup

--- server/server.js
+++ server/server.js
@@ -1,2 +1,21 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var fs = require('fs');
+var AWS = require('aws-sdk');
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'twig');
+app.set('twig options', {
+    strict_variables: false
+});
 
+app.use(express.static(__dirname + '/public'));
+server.listen(5000);
+var io = require('socket.io').listen(server);
+
+app.get('/', function(req, res) {
+    res.render('index.twig', {
+        message: "Hello World"
+    });
+})
